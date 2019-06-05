66e5e48b21d54e50d4f67cb3ad01bbdb32e53314
ThundaHorse <abeeeeekim@gmail.com>
2019-06-05 15:23:45 -0500

    added second page and added boostrap

--- app.rb
+++ app.rb
@@ -9,6 +9,9 @@ ENV =  YAML.load_file('config/application.yml')
 current_dir = Dir.pwd
 Dir["#{current_dir}/models/*.rb"].each { |file| require file }
 
+get '/home' do 
+  erb :home
+end 
 
 get '/info' do 
   response = HTTP.get("https://maps.googleapis.com/maps/api/directions/json?origin=215+w+ohio+st&destination=Niagra+Falls&key=#{ENV['API_KEY']}")
