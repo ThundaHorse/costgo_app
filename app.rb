57a403c7cc3ccc9a6d3bb6925104e90a0968f874
ThundaHorse <abeeeeekim@gmail.com>
2019-06-06 16:17:11 -0500

    integrated pry and updated home

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ require 'sinatra/activerecord'
 require 'http'
 require 'figaro'
 require 'yaml'
+require 'pry'
+
 ENV =  YAML.load_file('config/application.yml')
 
 current_dir = Dir.pwd
@@ -14,7 +16,12 @@ get '/home' do
 end 
 
 get '/info' do 
-  response = HTTP.get("https://maps.googleapis.com/maps/api/directions/json?origin=215+w+ohio+st&destination=Niagra+Falls&key=#{ENV['API_KEY']}")
+  origin = params.fetch('origin')
+  destination = params.fetch('destination')
+  response = HTTP.get("https://maps.googleapis.com/maps/api/directions/json?origin=#{params['origin']}&destination=#{params['destination']}&key=#{ENV['API_KEY']}")
   @data = response.parse
+
+
+
   erb :index
 end 
